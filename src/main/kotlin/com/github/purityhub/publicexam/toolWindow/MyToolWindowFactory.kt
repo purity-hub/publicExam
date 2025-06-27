51f8b5b58686d0d2e42dc152b92d5b21531a3ba6
GitHub Action <action@github.com>
2025-06-27 02:36:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/purityhub/publicexam/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/purityhub/publicexam/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.purityhub.publicexam.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.purityhub.publicexam.MyBundle
+import com.github.purityhub.publicexam.services.MyProjectService
 import javax.swing.JButton
 
 
